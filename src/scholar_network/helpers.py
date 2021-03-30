e25c1d77452486ca1d5c0e0a6f686145f612d253
Nick Anthony <nanthony007@gmail.com>
2021-03-30 12:39:20 -0400

    Change graph-building to use only author list.

--- src/scholar_network/helpers.py
+++ src/scholar_network/helpers.py
@@ -19,15 +19,19 @@ def build_graph() -> models.Graph:
     scholars = load_scholars()
     graph = models.Graph()
     for name in scholars.keys():
-        graph.add_node(models.Node(name))
         for pub in scholars[name]:
             co_authors = [c.strip() for c in pub.get("authors").split(",")]
             # TODO: add support for journal title as attribute of edge
             # TODO: want weight to track number of connections on each edge
+            # TODO: look at speed enhancements
             # journal = scholars[name].get("journal_title").strip()
             for co in co_authors:
-                if co == name:
-                    continue
                 graph.add_node(models.Node(co))
-                graph.add_edge(models.Edge(graph.get_node(name), graph.get_node(co)))
+                for other in co_authors:
+                    if other == co:
+                        continue
+                    graph.add_node(models.Node(other))
+                    graph.add_edge(
+                        models.Edge(graph.get_node(co), graph.get_node(other))
+                    )
     return graph
