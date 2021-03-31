af8a7e0d69714f44de465e36a15b9f9e8c770ce5
Nick Anthony <nanthony007@gmail.com>
2021-03-31 19:34:09 -0400

    Clean and reformat file.

--- src/scholar_network/helpers.py
+++ src/scholar_network/helpers.py
@@ -1,26 +1,29 @@
-from typing import Union
-from . import models
 import json
-import networkx as nx
-import pickle
+from typing import Union
+
+from . import models
 
 ENCODING = "utf-8-sig"
 
-# TODO: add functionality to compare authors/coauthor list and check
-# string similarity to avoid duplicate authors
-# --> probably build the name standardization package and use here
 
-
-def load_scholars_json():
-    with open("data/flattened-scraped.json", "r", encoding=ENCODING) as f:
+def load_publications():
+    with open("data/scraped.json", "r", encoding=ENCODING) as f:
         scholars = json.load(f)
     return scholars
+
+
+def append_pub_data_to_json(publication_info: list[dict[str, str]]):
+    with open("data/scraped.json", "r", encoding=ENCODING) as f:
+        data = json.load(f)
+    data.append(publication_info)
+    with open("data/scraped.json", "w", encoding=ENCODING) as f:
+        json.dump(data, f, indent=4, sort_keys=True)
 
 
 def build_graph(
     author1: Union[str, None] = None, author2: Union[str, None] = None
 ) -> models.Graph:
-    publications = load_scholars_json()
+    publications = load_publications()
     graph = models.Graph()
     for pub in publications:
         co_authors = set([c.strip() for c in pub.get("authors").split(",")])
@@ -44,37 +47,15 @@ def build_graph(
             for co in co_authors:
                 graph.add_node(models.Node(co))
                 for other in co_authors:
-                    graph.add_node(models.Node(co))
-                    for other in co_authors:
-                        if other == co:
-                            continue
-                        graph.add_node(models.Node(other))
-                        graph.add_edge(
-                            models.Edge(graph.get_node(co), graph.get_node(other))
-                        )
+                    if other == co:
+                        continue
+                    graph.add_node(models.Node(other))
+                    graph.add_edge(
+                        models.Edge(graph.get_node(co), graph.get_node(other))
+                    )
 
         # TODO: add support for journal title as attribute of edge
         # TODO: want weight to track number of connections on each edge
         # TODO: look at speed enhancements
         # journal = scholars[name].get("journal_title").strip()
-
     return graph
-
-
-def save_graph(connections: list[tuple[str, str]]):
-    G = nx.Graph()
-    G.add_edges_from(connections)
-    positions = nx.spring_layout(G)
-    with open("data/COP-graph.pkl", "wb") as f:
-        pickle.dump(G, f)
-    with open("data/COP-graph-positions.pkl", "wb") as f:
-        pickle.dump(positions, f)
-    return
-
-
-def load_graph_from_files() -> tuple[nx.Graph, nx.layout.spring_layout]:
-    with open("data/COP-graph.pkl", "rb") as f:
-        g = pickle.load(f)
-    with open("data/COP-graph-positions.pkl", "rb") as f:
-        positions = pickle.load(f)
-    return g, positions
