8e39b8e965a68a46534d7e42c8ea97153645cdf3
Nick Anthony <nanthony007@gmail.com>
2021-07-01 09:39:09 -0400

    Bump version

--- src/scholar_network/__init__.py
+++ src/scholar_network/__init__.py
@@ -3,7 +3,7 @@ This package is intended for people wanting to scrape Google Scholar
 to build graph networks of Google Scholar authors and identify network
 connections as opportunities for collaboration.
 """
-__version__ = "0.1.8"
+__version__ = "0.1.10"
 
 from .helpers import build_graph
 from .models import CustomCounter, Graph, Digraph, Edge, Node
