63469dae91525de08659c3d3ee32e147c8f61055
Nick Anthony <nanthony007@gmail.com>
2021-08-16 13:46:21 -0400

    Update url path to scrape all paginated records

--- src/scholar_network/scraping.py
+++ src/scholar_network/scraping.py
@@ -11,7 +11,7 @@ from selenium import webdriver
 from . import helpers
 
 
-def get_publication_data(author_id: str) -> list[dict[str, str]]:
+def get_publication_data(author_id: str, author_name: str = '') -> list[dict[str, str]]:
     """Retrives data from Google Scholar.
 
     This function is the primary web-utility that opens a new selenium
@@ -26,18 +26,25 @@ def get_publication_data(author_id: str) -> list[dict[str, str]]:
     """
     driver = webdriver.Safari()
     data = []
-    profile_link = f"https://scholar.google.com/citations?user={author_id}&hl=en&oi=ao"
-    driver.get(profile_link)
-    pub_elements = driver.find_elements_by_css_selector("a.gsc_a_at")
-    for pub in pub_elements:
-        pub_info_link = pub.get_attribute("data-href")
-        driver.get(f"https://scholar.google.com{pub_info_link}")
-        elements = driver.find_elements_by_class_name("gsc_vcd_value")
-        if len(elements) > 3:
-            data.append(
-                {"journal_title": elements[2].text, "authors": elements[0].text}
-            )
-        driver.back()
+    profile_link = f"https://scholar.google.com/citations?&hl=en&user={author_id}"
+    more_pubs = True
+    loop = 0
+    while more_pubs:
+        print(f"Scraping {author_name.title()} page {loop + 1}")
+        driver.get(f"{profile_link}&cstart={loop*100}&pagesize=100&view_op=list_works&sortby=pubdate")
+        more_pubs = driver.find_element_by_id('gsc_bpf_more').is_enabled()
+        if more_pubs:
+            loop += 1
+        pub_elements = driver.find_elements_by_css_selector("a.gsc_a_at")
+        for pub in pub_elements:
+            pub_info_link = pub.get_attribute("href")
+            driver.get(pub_info_link)
+            elements = driver.find_elements_by_class_name("gsc_oci_value")
+            if len(elements) > 3:
+                data.append(
+                    {"journal_title": elements[2].text, "authors": elements[0].text}
+                )
+            driver.back()
     driver.close()
     return data
 
@@ -49,6 +56,6 @@ def scrape_single_author(scholar_id: str, scholar_name: str = ''):
         scholar_id (str): Google Scholar ID of the author information to extract.
         scholar_name (str, optional): Name of the scholar. Defaults to ''.
     """
-    pub_data = get_publication_data(scholar_id)
+    pub_data = get_publication_data(scholar_id, scholar_name)
     helpers.append_pub_data_to_json(pub_data)
     print(f"Wrote {scholar_name if scholar_name else scholar_id} to file.")
